0fd03d4124d22267395c9f29076a996c6bf95dae
minipong-dev <60086046+minipong-dev@users.noreply.github.com>
2022-09-11 23:46:57 -0700

    Add files via upload

--- nft-minting-app-main (magicprinters)/src/App.js
+++ nft-minting-app-main (magicprinters)/src/App.js
@@ -356,6 +356,24 @@ function App() {
     };
 
     useEffect(() => {
+        const items = document.querySelectorAll(".accordion button");
+
+        function toggleAccordion() {
+        const itemToggle = this.getAttribute('aria-expanded');
+        
+        for (i = 0; i < items.length; i++) {
+            items[i].setAttribute('aria-expanded', 'false');
+        }
+        
+        if (itemToggle == 'false') {
+            this.setAttribute('aria-expanded', 'true');
+        }
+        }
+
+        items.forEach(item => item.addEventListener('click', toggleAccordion));
+    }, []);
+
+    useEffect(() => {
         getConfig();
     }, []);
 
@@ -488,7 +506,7 @@ function App() {
                                 style={{
                                     textAlign: "center",
                                     fontFamily: "exo2black",
-                                    fontSize: 20,
+                                    fontSize: 18,
                                     color: "var(--primary-text)",
                                     margin: "5px auto",
                                 }}
@@ -503,7 +521,7 @@ function App() {
                                 style={{
                                     textAlign: "center",
                                     fontFamily: "exo2black",
-                                    fontSize: 20,
+                                    fontSize: 18,
                                     color: "var(--primary-text)",
                                     margin: "5px auto",
                                 }}
@@ -518,7 +536,7 @@ function App() {
                                 style={{
                                     textAlign: "center",
                                     fontFamily: "exo2black",
-                                    fontSize: 20,
+                                    fontSize: 18,
                                     color: "var(--primary-text)",
                                     margin: "5px auto",
                                 }}
@@ -529,6 +547,40 @@ function App() {
                     </div>
                     
                     <s.SpacerLarge/>
+
+                    <h2>Frequently Asked Questions</h2>
+                    <div class="accordion">
+                        <div class="accordion-item">
+                        <button id="accordion-button-1" aria-expanded="false"><span class="accordion-title">Why is the moon sometimes out during the day?</span><span class="icon" aria-hidden="true"></span></button>
+                        <div class="accordion-content">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+                        </div>
+                        </div>
+                        <div class="accordion-item">
+                        <button id="accordion-button-2" aria-expanded="false"><span class="accordion-title">Why is the sky blue?</span><span class="icon" aria-hidden="true"></span></button>
+                        <div class="accordion-content">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+                        </div>
+                        </div>
+                        <div class="accordion-item">
+                        <button id="accordion-button-3" aria-expanded="false"><span class="accordion-title">Will we ever discover aliens?</span><span class="icon" aria-hidden="true"></span></button>
+                        <div class="accordion-content">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+                        </div>
+                        </div>
+                        <div class="accordion-item">
+                        <button id="accordion-button-4" aria-expanded="false"><span class="accordion-title">How much does the Earth weigh?</span><span class="icon" aria-hidden="true"></span></button>
+                        <div class="accordion-content">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+                        </div>
+                        </div>
+                        <div class="accordion-item">
+                        <button id="accordion-button-5" aria-expanded="false"><span class="accordion-title">How do airplanes stay up?</span><span class="icon" aria-hidden="true"></span></button>
+                        <div class="accordion-content">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+                        </div>
+                        </div>
+                    </div>
 
                     <ResponsiveWrapper flex={1} style={{ padding: 25 }} test>
                         <s.Container flex={1} jc={"center"} ai={"center"}>
@@ -826,6 +878,7 @@ function App() {
                 </s.Container >
             </div>
         </s.Screen >
+
     );
 }
 
